da4c544eae479c17edd5d641f826ff3f774319c8
Cory Eighan <Ninja-Panda@Corys-MacBook-Pro.local>
2016-07-03 17:19:48 -0700

    Tableview controller now displays static data.

--- Workout Tracker/Exercises/ExerciseTableViewController.swift
+++ Workout Tracker/Exercises/ExerciseTableViewController.swift
@@ -10,24 +10,33 @@ import UIKit
 
 class ExerciseTableViewController: UITableViewController {
     
+    var exercise = [String]()
+    var newExercise: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        exercise = ["Incline Bench Press","Pull ups","Bicep curls"]
+    }
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // 1
+        
         return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0;
+        return exercise.count
         
     }
     
     
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        // 3
-////        let cell = tableView.dequeueReusableCellWithIdentifier("carCell", forIndexPath: indexPath) as! UITableViewCell
-////        
-////        cell.textLabel!.text = [indexPath.row]  
-////        return cell
-//    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("exerciseCell", forIndexPath: indexPath)
+        
+        cell.textLabel!.text = exercise[indexPath.row]
+        return cell
+    }
 
 }
